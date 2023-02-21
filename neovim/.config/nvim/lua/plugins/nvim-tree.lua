84002b1c677c1ccc7d8195b75bb8640df4df1a51
Eleazar Pequeno <edpu0033@gmail.com>
2023-02-21 00:11:50 -0500

    Add nvim-tree plugin

---
+++ neovim/.config/nvim/lua/plugins/nvim-tree.lua
@@ -0,0 +1,12 @@
+return {
+  'nvim-tree/nvim-tree.lua',
+  dependencies = {
+    'nvim-tree/nvim-web-devicons', -- optional, for file icons
+  },
+  tag = 'nightly', -- optional, updated every week. (see issue #1193)
+  config = function ()
+    vim.g.loaded_netrw = 1
+    vim.g.loaded_netrwPlugin = 1
+    require('nvim-tree').setup()
+  end
+}
