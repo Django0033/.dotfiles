f8563139af3c202e857944a760994edb29110df4
Eleazar Pequeño <edpu0033@gmail.com>
2023-02-25 14:10:49 -0500

    Set <leader>dq to vim.diagnostic.setloclist

--- neovim/.config/nvim/lua/mappings/treesitter-mappings.lua
+++ neovim/.config/nvim/lua/mappings/treesitter-mappings.lua
@@ -5,5 +5,5 @@ local map = vim.keymap.set
 map('n', '[d', vim.diagnostic.goto_prev)
 map('n', ']d', vim.diagnostic.goto_next)
 map('n', '<leader>e', vim.diagnostic.open_float)
--- vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist)
+vim.keymap.set('n', '<leader>dq', vim.diagnostic.setloclist)
 -- [[]]
