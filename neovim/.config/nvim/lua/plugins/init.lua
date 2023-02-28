b11df6d84f6c375ff48b7d0255acae5548f2da59
Eleazar Pequeno <edpu0033@gmail.com>
2023-02-28 02:44:49 -0500

    Add vim-tmux-navigator plugin

--- neovim/.config/nvim/lua/plugins/init.lua
+++ neovim/.config/nvim/lua/plugins/init.lua
@@ -1,19 +1,20 @@
 return {
-  'tpope/vim-surround',
-  'tpope/vim-repeat',
-  -- Git related plugins
-  'tpope/vim-rhubarb',
+      'christoomey/vim-tmux-navigator',
+      'tpope/vim-surround',
+      'tpope/vim-repeat',
+      -- Git related plugins
+      'tpope/vim-rhubarb',
 
-  -- Detect tabstop and shiftwidth automatically
-  'tpope/vim-sleuth',
+      -- Detect tabstop and shiftwidth automatically
+      'tpope/vim-sleuth',
 
-  -- Useful plugin to show you pending keybinds.
-  { 'folke/which-key.nvim', opts = {} },
+      -- Useful plugin to show you pending keybinds.
+      { 'folke/which-key.nvim',  opts = {} },
 
-  -- "gc" to comment visual regions/lines
-  { 'numToStr/Comment.nvim', opts = {} },
-  'renerocksai/calendar-vim',
-  'nvim-tree/nvim-web-devicons',
-  'ap/vim-css-color',
-  'nvim-telescope/telescope-hop.nvim'
-}, {}
+      -- "gc" to comment visual regions/lines
+      { 'numToStr/Comment.nvim', opts = {} },
+      'renerocksai/calendar-vim',
+      'nvim-tree/nvim-web-devicons',
+      'ap/vim-css-color',
+      'nvim-telescope/telescope-hop.nvim'
+    }, {}
