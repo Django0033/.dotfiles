cde277150ccaff0511466d7f58e0b1263e19beff
Eleazar Pequeno <edpu0033@gmail.com>
2023-03-09 20:52:38 -0500

    Load friendly-snippets

--- neovim/.config/nvim/lua/plugins/luasnip.lua
+++ neovim/.config/nvim/lua/plugins/luasnip.lua
@@ -15,6 +15,9 @@ return {
       updateevents = 'TextChanged, TextChangedI',
       enable_autosnippets = true,
     }
+
+    require('luasnip.loaders.from_vscode').lazy_load()
+
     luasnip.add_snippets(
       'all', {
       snippet('expand', {
