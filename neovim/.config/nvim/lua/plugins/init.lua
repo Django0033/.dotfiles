a57363c330fcf3e7cd39555f0c5cdb717736496f
Eleazar Pequeño <edpu0033@gmail.com>
2023-03-10 17:48:44 -0500

    Remove vim-rhubarb and delete comments

--- neovim/.config/nvim/lua/plugins/init.lua
+++ neovim/.config/nvim/lua/plugins/init.lua
@@ -2,16 +2,11 @@ return {
       'christoomey/vim-tmux-navigator',
       'tpope/vim-surround',
       'tpope/vim-repeat',
-      -- Git related plugins
-      'tpope/vim-rhubarb',
 
       -- Detect tabstop and shiftwidth automatically
       'tpope/vim-sleuth',
 
-      -- Useful plugin to show you pending keybinds.
       { 'folke/which-key.nvim',  opts = {} },
-
-      -- "gc" to comment visual regions/lines
       { 'numToStr/Comment.nvim', opts = {} },
       'renerocksai/calendar-vim',
       'nvim-tree/nvim-web-devicons',
