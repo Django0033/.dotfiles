19ea7cb4a26badfb0ea2e6a91a30699d518ddd95
Eleazar Pequeno <edpu0033@gmail.com>
2023-03-16 00:45:59 -0500

    Add chatgpt plugin

---
+++ neovim/.config/nvim/lua/plugins/chatgpt.lua
@@ -0,0 +1,19 @@
+return {
+  'jackMort/ChatGPT.nvim',
+  dependencies = {
+    'MunifTanjim/nui.nvim',
+    'nvim-lua/plenary.nvim',
+    'nvim-telescope/telescope.nvim',
+  },
+  config = function()
+    local ok, chatgpt = pcall(require, 'chatgpt')
+    if not ok then
+      return
+    end
+    chatgpt.setup({
+      keymaps = {
+        submit = '<CR>',
+      }
+    })
+  end
+}
