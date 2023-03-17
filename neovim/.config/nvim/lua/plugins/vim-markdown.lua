f247892d44050e07d84bdfb4d2e4b4d89b5c35e2
Eleazar Pequeño <edpu0033@gmail.com>
2023-03-17 17:54:34 -0500

    Add some vim-markdown configs

--- neovim/.config/nvim/lua/plugins/vim-markdown.lua
+++ neovim/.config/nvim/lua/plugins/vim-markdown.lua
@@ -7,5 +7,10 @@ return {
     local status, vim_markdown = pcall(require, 'vim-markdown')
     if not status then return end
     vim_markdown.setup {}
+
+    local map = vim.keymap.set
+    local opts = { noremap = true, silent = true }
+
+    map('n', '<leader>mt', '<cmd>Toc<CR>', opts)
   end
 }
