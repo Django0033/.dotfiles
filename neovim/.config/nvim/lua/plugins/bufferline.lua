519b0568a08fc8750f992a2516f4a179dc29c374
Eleazar Pequeno <edpu0033@gmail.com>
2023-03-21 00:53:17 -0500

    Add mappings
    Add mappings to pin buffers and to delete the unpinned ones.

--- neovim/.config/nvim/lua/plugins/bufferline.lua
+++ neovim/.config/nvim/lua/plugins/bufferline.lua
@@ -4,13 +4,19 @@ return {
   dependencies = {
     'nvim-tree/nvim-web-devicons',
   },
-  config = function ()
+  event = 'VeryLazy',
+  keys = {
+    { '<leader>bp', '<cmd>BufferLineTogglePin<CR>',            desc = 'Toggle Pin' },
+    { '<leader>bP', '<cmd>BufferLineGroupClose ungrouped<CR>', desc = 'Delete non-pinned buffers' },
+  },
+  config = function()
     local status, bufferline = pcall(require, 'bufferline')
     if not status then
       return
     end
-    bufferline.setup{
+    bufferline.setup {
       options = {
+        diagnostics = 'nvim_lsp',
         separator_style = 'slant',
         always_show_bufferline = false,
         show_buffer_close_icons = false,
@@ -18,5 +24,4 @@ return {
       }
     }
   end
-
 }
