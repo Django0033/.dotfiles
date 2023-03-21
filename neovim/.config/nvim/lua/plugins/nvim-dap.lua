25efe5ef1c3c01b909892c555de8951c093dbacf
Eleazar Pequeno <edpu0033@gmail.com>
2023-03-21 00:56:19 -0500

    Change mappings
    Change toggle_breakpoint to <leader>B and condition breakpoint to
    <leader>cB

--- neovim/.config/nvim/lua/plugins/nvim-dap.lua
+++ neovim/.config/nvim/lua/plugins/nvim-dap.lua
@@ -6,12 +6,12 @@ return {
     'nvim-telescope/telescope-dap.nvim',
   },
   keys = {
-    { '<F5>',      ':lua require\'dap\'.continue()<CR>',                                               desc = 'Continue' },
-    { '<F10>',     ':lua require\'dap\'.step_over()<CR>',                                              desc = 'Step over' },
-    { '<F11>',     ':lua require\'dap\'.step_into()<CR>',                                              desc = 'Step into' },
-    { '<F12>',     ':lua require\'dap\'.step_out()<CR>',                                               desc = 'Step out' },
-    { '<leader>b', ':lua require\'dap\'.toggle_breakpoint()<CR>',                                      desc = 'Toggle Breakpoint' },
-    { '<leader>B', ':lua require\'dap\'.set_breakpoint(vim.fn.input(\'Breakpoint condition: \'))<CR>', desc = 'Set condtional breakpoint' },
+    { '<F5>',       ':lua require\'dap\'.continue()<CR>',                                               desc = 'Continue' },
+    { '<F10>',      ':lua require\'dap\'.step_over()<CR>',                                              desc = 'Step over' },
+    { '<F11>',      ':lua require\'dap\'.step_into()<CR>',                                              desc = 'Step into' },
+    { '<F12>',      ':lua require\'dap\'.step_out()<CR>',                                               desc = 'Step out' },
+    { '<leader>B',  ':lua require\'dap\'.toggle_breakpoint()<CR>',                                      desc = 'Toggle Breakpoint' },
+    { '<leader>cB', ':lua require\'dap\'.set_breakpoint(vim.fn.input(\'Breakpoint condition: \'))<CR>', desc = 'Set condtional breakpoint' },
   },
   config = function()
     local ok, dap = pcall(require, 'dap')
@@ -36,6 +36,5 @@ return {
       return
     end
     dapPython.setup('~/.virtualenvs/debugpy/bin/python')
-
   end
 }
