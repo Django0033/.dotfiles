d7362954945c6f0eb3e32f7ae4ded6dd1c2d34fb
Eleazar Pequeno <edpu0033@gmail.com>
2023-03-21 00:54:50 -0500

    Improve leap mappings

--- neovim/.config/nvim/lua/plugins/leap.lua
+++ neovim/.config/nvim/lua/plugins/leap.lua
@@ -1,6 +1,20 @@
 return {
   'ggandor/leap.nvim',
-  config = function ()
-   require('leap').add_default_mappings()
+  keys = {
+    { 's',  mode = { 'n', 'x', 'o' }, desc = 'Leap forward to' },
+    { 'S',  mode = { 'n', 'x', 'o' }, desc = 'Leap backward to' },
+    { 'gs', mode = { 'n', 'x', 'o' }, desc = 'Leap from windows' },
+  },
+  config = function(_, opts)
+    local ok, leap = pcall(require, 'leap')
+    if not ok then
+      return
+    end
+    for k, v in pairs(opts) do
+      leap.opts[k] = v
+    end
+    leap.add_default_mappings(true)
+    vim.keymap.del({ 'x', 'o' }, 'x')
+    vim.keymap.del({ 'x', 'o' }, 'X')
   end
 }
