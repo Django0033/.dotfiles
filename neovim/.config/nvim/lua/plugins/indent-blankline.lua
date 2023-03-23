35a64bd3ddd08d33455b94a7f3b7d1b00dfc2ae5
Eleazar Pequeno <edpu0033@gmail.com>
2023-03-23 01:59:12 -0500

    Set context indent highlight

--- neovim/.config/nvim/lua/plugins/indent-blankline.lua
+++ neovim/.config/nvim/lua/plugins/indent-blankline.lua
@@ -1,10 +1,18 @@
 return {
-  -- Add indentation guides even on blank lines
   'lukas-reineke/indent-blankline.nvim',
-  -- Enable `lukas-reineke/indent-blankline.nvim`
-  -- See `:help indent_blankline.txt`
-  opts = {
-    char = '┊',
-    show_trailing_blankline_indent = false,
-  },
+  config = function()
+    local ok, indent_blankline = pcall(require, 'indent_blankline')
+    if not ok then
+      return
+    end
+
+    vim.opt.list = true
+
+    indent_blankline.setup {
+      show_end_of_line = true,
+      space_char_blankline = ' ',
+      show_current_context = true,
+      show_current_context_start = true,
+    }
+  end,
 }
