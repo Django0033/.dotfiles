19ef5d57834fad2195a7b1745e9fe1870842c25c
Eleazar Pequeno <edpu0033@gmail.com>
2023-03-26 20:33:19 -0500

    Switch to mini.surround

--- neovim/.config/nvim/lua/plugins/init.lua
+++ neovim/.config/nvim/lua/plugins/init.lua
@@ -1,14 +1,10 @@
 return {
-      'christoomey/vim-tmux-navigator',
-      'tpope/vim-surround',
-      'tpope/vim-repeat',
-
-      -- Detect tabstop and shiftwidth automatically
-      'tpope/vim-sleuth',
-      { 'folke/which-key.nvim',        opts = {} },
-      { 'nvim-tree/nvim-web-devicons', lazy = true },
-      'ap/vim-css-color',
-      'ThePrimeagen/vim-be-good',
-      'dhruvasagar/vim-table-mode',
-      { 'chrisbra/csv.vim', ft = 'csv' },
-    }, {}
+	"christoomey/vim-tmux-navigator",
+	"tpope/vim-repeat",
+	"tpope/vim-sleuth",
+	{ "folke/which-key.nvim", opts = {} },
+	{ "nvim-tree/nvim-web-devicons", lazy = true },
+	"ap/vim-css-color",
+	"dhruvasagar/vim-table-mode",
+	{ "chrisbra/csv.vim", ft = "csv" },
+}, {}
