c0f7eaa257efb2f087837c59b98d558a5c9ac179
Eleazar Pequeno <edpu0033@gmail.com>
2023-03-26 20:31:13 -0500

    Switch to mini.comment

---
+++ neovim/.config/nvim/lua/plugins/mini-comment.lua
@@ -0,0 +1,17 @@
+return {
+  "echasnovski/mini.comment",
+  dependencies = {
+    { "JoosepAlviste/nvim-ts-context-commentstring", lazy = true },
+  },
+  event = "VeryLazy",
+  opts = {
+    hooks = {
+      pre = function()
+        require("ts_context_commentstring.internal").update_commentstring({})
+      end,
+    },
+  },
+  config = function(_, opts)
+    require("mini.comment").setup(opts)
+  end,
+}
