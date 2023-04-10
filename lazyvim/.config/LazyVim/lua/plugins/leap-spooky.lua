2ea196f90499f7c4f6e067dd25da4c2b46575bac
Eleazar Pequeno <edpu0033@gmail.com>
2023-04-10 01:59:31 -0500

    Make leap-spooky not lazy

--- lazyvim/.config/LazyVim/lua/plugins/leap-spooky.lua
+++ lazyvim/.config/LazyVim/lua/plugins/leap-spooky.lua
@@ -1,13 +1,13 @@
 return {
-  'ggandor/leap-spooky.nvim',
-  lazy = true,
+  "ggandor/leap-spooky.nvim",
+  -- lazy = true,
   config = function()
-    local ok, leap_spooky = pcall(require, 'leap-spooky')
+    local ok, leap_spooky = pcall(require, "leap-spooky")
     if not ok then
       return
     end
-    leap_spooky.setup {
-      paste_on_remote_yank = true
-    }
-  end
+    leap_spooky.setup({
+      paste_on_remote_yank = true,
+    })
+  end,
 }
