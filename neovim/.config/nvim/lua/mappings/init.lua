993bdd2197ebaf15b44b03b3f3b7fac0fd6d795d
Eleazar Pequeño <edpu0033@gmail.com>
2023-04-14 12:16:18 -0500

    Modify mappings

--- neovim/.config/nvim/lua/mappings/init.lua
+++ neovim/.config/nvim/lua/mappings/init.lua
@@ -7,10 +7,31 @@ local noremap = { noremap = true }
 -- See `:help vim.keymap.set()`
 map({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
 
--- Remap for dealing with word wrap
+-- Better up and down / Remap for dealing with word wrap
 map('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
 map('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
 
+-- Move to split
+-- map('n', '<C-l>', '<C-w>l', opts)
+-- map('n', '<C-h>', '<C-w>h', opts)
+-- map('n', '<C-j>', '<C-w>j', opts)
+-- map('n', '<C-k>', '<C-w>k', opts)
+
+-- Resize split
+map('n', '<leader>+', ':vertical resize +5<CR>', opts)
+map('n', '<leader>-', ':vertical resize -5<CR>', opts)
+
+-- Moving text
+map('v', 'J', ':m \'>+1<CR>gv=gv', opts)
+map('v', 'K', ':m \'<-2<CR>gv=gv', opts)
+map('v', '<', '<gv', opts)
+map('v', '>', '>gv', opts)
+
+-- Buffer controls
+map('n', '<S-l>', ':bn<CR>', opts)
+map('n', '<S-h>', ':bp<CR>', opts)
+map('n', '<leader>q', ':bdel<CR>', opts)
+-- map('n', '<leader>bq', ":%bd\|e#\|bd#<CR>", opts)
 
 -- map('i', 'ii', '<ESC><ESC>', opts)
 -- map('v', 'ii', '<ESC><ESC>', opts)
@@ -29,12 +50,6 @@ map('n', 'c#', '#``cgN', opts)
 map('n', '<leader><TAB>t', ':tabe<CR>', opts)
 map('n', '<leader><TAB>n', ':tabnext<CR>', opts)
 
--- Buffer controls
-map('n', '<S-l>', ':bn<CR>', opts)
-map('n', '<S-h>', ':bp<CR>', opts)
-map('n', '<leader>q', ':bdel<CR>', opts)
--- map('n', '<leader>bq', ":%bd\|e#\|bd#<CR>", opts)
-
 -- Split controls
 -- map('n', '<leader>vs', ':vnew<CR>', opts)
 map('n', '<leader>vs', function()
@@ -47,16 +62,10 @@ map('n', '<leader>hs', function()
   vim.cmd [[split]]
   vim.cmd [[enew]]
 end, opts)
--- map('n', '<C-l>', '<C-w>l', opts)
--- map('n', '<C-h>', '<C-w>h', opts)
--- map('n', '<C-j>', '<C-w>j', opts)
--- map('n', '<C-k>', '<C-w>k', opts)
-map('n', '<leader>+', ':vertical resize +5<CR>', opts)
-map('n', '<leader>-', ':vertical resize -5<CR>', opts)
+
 map('n', '<leader>M', '<C-w>T', opts)
 map('n', '<leader>Rv', 'mAZZ<C-w>v`A', opts)
 map('n', '<leader>Rh', 'mAZZ<C-w>s`A', opts)
-
 
 -- Keep your cursor centered while jumping through search results or joining
 -- lines.
@@ -80,11 +89,6 @@ map('i', '\'', '\'<C-g>u', opts)
 map('i', '"', '"<C-g>u', opts)
 map('i', ':', ':<C-g>u', opts)
 
--- Moving text
-map('v', 'J', ':m \'>+1<CR>gv=gv', opts)
-map('v', 'K', ':m \'<-2<CR>gv=gv', opts)
-map('v', '<', '<gv', opts)
-map('v', '>', '>gv', opts)
 
 -- [[ Highlight on yank ]]
 -- See `:help vim.highlight.on_yank()`
