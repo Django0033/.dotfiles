ae4387165a46db1dfe939e696441036dc8fb099b
Eleazar Pequeño <edpu0033@gmail.com>
2023-04-17 13:52:48 -0500

    Set buffer line always show to false.

--- neovim/.config/nvim/lua/plugins/bufferline.lua
+++ neovim/.config/nvim/lua/plugins/bufferline.lua
@@ -18,7 +18,7 @@ return {
       options = {
         diagnostics = 'nvim_lsp',
         separator_style = 'slant',
-        always_show_bufferline = true,
+        always_show_bufferline = false,
         show_buffer_close_icons = false,
         show_close_icon = false,
       }
