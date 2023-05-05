3036631bcaee5f8225e1123ffed25e82e6272171
Eleazar Pequeno <edpu0033@gmail.com>
2023-05-05 00:16:56 -0500

    Add nvimgelion colorscheme

--- neovim/.config/nvim/lua/plugins/colorschemes.lua
+++ neovim/.config/nvim/lua/plugins/colorschemes.lua
@@ -35,7 +35,7 @@ return {
       vim.g.gruvbox_material_foreground = 'original'
       vim.g.gruvbox_material_better_performance = 1
 
-      vim.cmd('colorscheme gruvbox-material')
+      -- vim.cmd('colorscheme gruvbox-material')
 
       -- vim.g.lightline = { colorscheme = 'gruvbox_material' }
     end
