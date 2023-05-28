983a65733e3ee77523318b5b4ed99d7d75585b0c
Eleazar Pequeno <edpu0033@gmail.com>
2023-05-28 00:44:31 -0500

    [nvim] Set neorg default binds

--- neovim/.config/nvim/lua/plugins/neorg.lua
+++ neovim/.config/nvim/lua/plugins/neorg.lua
@@ -3,13 +3,18 @@ return {
   build = ":Neorg sync-parsers",
   opts = {
     load = {
-      ["core.defaults"] = {}, -- Loads default behaviour
+      ["core.defaults"] = {},  -- Loads default behaviour
       ["core.concealer"] = {}, -- Adds pretty icons to your documents
-      ["core.dirman"] = { -- Manages Neorg workspaces
+      ["core.dirman"] = {      -- Manages Neorg workspaces
         config = {
           workspaces = {
             notes = "~/notes",
           },
+        },
+      },
+      ["core.keybinds"] = {
+        config = {
+          default_keybinds = true,
         },
       },
     },
