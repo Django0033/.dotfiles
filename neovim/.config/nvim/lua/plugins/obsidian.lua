3446c6ba6ea065b02b7834c3f897593ec6ed2382
Eleazar Pequeno <edpu0033@gmail.com>
2025-06-04 14:20:08 -0500

    Add template folder and modify keymaps for obsidian plugin

--- neovim/.config/nvim/lua/plugins/obsidian.lua
+++ neovim/.config/nvim/lua/plugins/obsidian.lua
@@ -19,15 +19,28 @@ return {
                 path = '~/Sync/Roleplay',
             },
         },
-        daily_notes = {
-            template = 'new_note',
+
+        -- daily_notes = {
+        --     template = 'new_note',
+        -- },
+
+        templates = {
+            folder = 'templates'
         },
+
     },
 
     keys = {
+        {'<leader>od', function()
+            vim.cmd '!rm %'
+            vim.cmd 'bdel'
+        end, desc = 'Obsidian Delete'},
+
         {'<leader>on', function() vim.cmd 'ObsidianNew' end, desc = 'Obsidian New Note'},
         {'<leader>oq', function() vim.cmd 'ObsidianQuickSwitch' end, desc = 'Obsidian Quick Switch'},
-        {'<leader>ow', function() vim.cmd 'ObsidianWorkspace' end, desc = 'Obsidian Workspace'},
         {'<leader>or', function() vim.cmd 'ObsidianRename' end, desc = 'Obsidian Rename'},
+        {'<leader>os', function() vim.cmd 'ObsidianSearch' end, desc = 'Obsidian Search'},
+        {'<leader>ot', function() vim.cmd 'ObsidianTemplate' end, desc = 'Obsidian Template'},
+        {'<leader>ow', function() vim.cmd 'ObsidianWorkspace' end, desc = 'Obsidian Workspace'},
     },
 }
