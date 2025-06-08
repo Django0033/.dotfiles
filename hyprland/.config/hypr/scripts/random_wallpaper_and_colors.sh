adf8edc4811a11814cd00eade9866494a33ac190
Eleazar Pequeno <edpu0033@gmail.com>
2025-06-07 20:16:42 -0500

    Add matugen to the random_wallpaper_and_colors script
    Also corrected some typos.

--- hyprland/.config/hypr/scripts/random_wallpaper_and_colors.sh
+++ hyprland/.config/hypr/scripts/random_wallpaper_and_colors.sh
@@ -6,17 +6,19 @@ sleep 1
 WALLPAPER_DIR="$HOME/Pictures/wallpapers/"
 CURRENT_WALL=$(hyprctl hyprpaper listloaded)
 
-WALLPAPEL=$(find "$WALLPAPER_DIR" -type f ! -name "$(basename "$CURRENT_WALL")" | shuf -n 1)
+WALLPAPER=$(find "$WALLPAPER_DIR" -type f ! -name "$(basename "$CURRENT_WALL")" | shuf -n 1)
 
 hyprpaper & disown
 
 sleep 1
 
-hyprctl hyprpaper reload ,"$WALLPAPEL"
+hyprctl hyprpaper reload ,"$WALLPAPER"
 
 sleep 1
 
-wallust -q run "$WALLPAPEL"
+wallust -q run "$WALLPAPER"
+
+matugen image "$WALLPAPER"
 
 killall waybar && waybar & disown
 
