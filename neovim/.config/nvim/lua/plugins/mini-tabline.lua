22c39c51165d682b455082482be47de16a609cd6
Eleazar Pequeno <edpu0033@gmail.com>
2025-09-28 21:26:55 -0500

    Make mini-tabline file more readable

--- neovim/.config/nvim/lua/plugins/mini-tabline.lua
+++ neovim/.config/nvim/lua/plugins/mini-tabline.lua
@@ -1,13 +1,11 @@
 return {
     'nvim-mini/mini.tabline',
+
     dependencies = {
         {'nvim-mini/mini.icons', opts = {}}
     },
+
     version = false,
-    opts = {
-        function(buf_id, label)
-            local suffix = vim.bo[buf_id].modified and '+' or ''
-            return MiniTabline.default_format(buf_id,label) .. suffix
-        end,
-    },
+
+    opts = {},
 }
