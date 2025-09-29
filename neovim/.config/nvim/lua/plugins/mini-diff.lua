9206b61f72cd5adc1d322d30b6044bb7a0a90460
Eleazar Pequeno <edpu0033@gmail.com>
2025-09-28 21:30:34 -0500

    Add mini-diff plugin

---
+++ neovim/.config/nvim/lua/plugins/mini-diff.lua
@@ -0,0 +1,13 @@
+return {
+    'nvim-mini/mini.diff',
+    version = false,
+    lazy = false,
+
+    opts = {},
+
+    keys = {
+        {'<leader>dn', '<cmd>lua MiniDiff.config.view.style = "number"; vim.cmd("edit")<CR>', desc = 'Diff Numbers'},
+        {'<leader>do', '<cmd>lua MiniDiff.toggle_overlay()<CR>', desc = 'Diff Overlay'},
+        {'<leader>ds', '<cmd>lua MiniDiff.config.view.style = "sign"; vim.cmd("edit")<CR>', desc = 'Diff Signs'},
+    }
+}
