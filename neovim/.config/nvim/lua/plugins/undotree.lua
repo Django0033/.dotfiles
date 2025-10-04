928939ad369eed9be87bf71189525d9fa7cc3640
Eleazar Pequeno <edpu0033@gmail.com>
2025-10-04 10:25:15 -0500

    Set window to solid background

--- neovim/.config/nvim/lua/plugins/undotree.lua
+++ neovim/.config/nvim/lua/plugins/undotree.lua
@@ -4,6 +4,9 @@ return {
   ---@type UndoTreeCollector.Opts
   opts = {
     -- your options
+    window = {
+        winblend = 0,
+    }
   },
   keys = { -- load the plugin only when using it's keybinding:
     { "<leader>u", "<cmd>lua require('undotree').toggle()<cr>", desc = 'Undo Tree' },
