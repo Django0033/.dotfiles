749b64d7eb31f35b7d8cc17f827c227f63e4c422
Eleazar Pequeno <edpu0033@gmail.com>
2025-10-05 11:38:34 -0500

    Update nvim-treesitter config

--- neovim/.config/nvim/lua/plugins/nvim-treesitter.lua
+++ neovim/.config/nvim/lua/plugins/nvim-treesitter.lua
@@ -1,30 +1,35 @@
 return {
-    {
-        'nvim-treesitter/nvim-treesitter',
-        opt = {
-            ensure_installed = {
-                'javascript',
-                'typescript',
-                'css',
-                'gitignore',
-                'graphql',
-                'http',
-                'json',
-                'scss',
-                'sql',
-                'vim',
-                'lua',
-                'markdown',
-                'markdown_inline',
-            },
-            highlight = {
-                enable = true,
-            },
-            query_linter = {
-                enable = true,
-                use_virtual_text = true,
-                lint_events = {'BufWrite', 'CursorHold'},
-            },
+    'nvim-treesitter/nvim-treesitter',
+    build = ':TSUpdate',
+    lazy = false,
+
+    opt = {
+        ensure_installed = {
+            'css',
+            'gitignore',
+            'graphql',
+            'http',
+            'javascript',
+            'json',
+            'lua',
+            'markdown',
+            'markdown_inline',
+            'python',
+            'scss',
+            'sql',
+            'typescript',
+            'vim',
+        },
+
+        sync_install = true,
+
+        highlight = {
+            enable = true,
+        },
+        query_linter = {
+            enable = true,
+            use_virtual_text = true,
+            lint_events = {'BufWrite', 'CursorHold'},
         },
     },
 }
