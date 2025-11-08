e2e30abbbb52a9fe57e2d623d4c4d481f71eea73
Eleazar Pequeno <edpu0033@gmail.com>
2025-11-07 22:27:41 -0500

    Add no auto comments autocmd

--- neovim/.config/nvim/lua/config/settings.lua
+++ neovim/.config/nvim/lua/config/settings.lua
@@ -122,3 +122,12 @@ vim.diagnostic.config({
     virtual_text = false,
     virtual_lines = {only_current_line = true}
 })
+
+-- [[ No auto continue comments on new line ]]
+vim.api.nvim_create_autocmd('FileType', {
+    group = vim.api.nvim_create_augroup('no_auto_comment', {}),
+    callback = function ()
+        vim.opt_local.formatoptions:remove({'c', 'r', 'o'})
+    end,
+})
+-- [[]]
