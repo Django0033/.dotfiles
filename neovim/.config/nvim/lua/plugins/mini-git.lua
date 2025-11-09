736157f1f973f6b4643fbb828eb2aba857cd89ba
Eleazar Pequeno <edpu0033@gmail.com>
2025-11-08 21:57:51 -0500

    Refactor mini-git config file

--- neovim/.config/nvim/lua/plugins/mini-git.lua
+++ neovim/.config/nvim/lua/plugins/mini-git.lua
@@ -1,21 +1,21 @@
 return {
-    'nvim-mini/mini-git',
+    "nvim-mini/mini-git",
     version = false,
     lazy = false,
 
     opts = {},
 
-    config = function ()
-        local ok, mini_git = pcall(require,'mini.git')
+    config = function()
+        local _, mini_git = pcall(require, "mini.git")
         mini_git.setup()
     end,
 
     keys = {
-        {'<leader>gP', '<cmd>Git push<CR>', desc = 'Git Push'},
-        {'<leader>ga', '<cmd>Git add *<CR>', desc = 'Git Add'},
-        {'<leader>gc', '<cmd>horizontal Git commit<CR>', desc = 'Git Commit'},
-        {'<leader>gh', '<cmd>vertical lua MiniGit.show_at_cursor()<CR>', desc = 'Git Show'},
-        {'<leader>gl', '<cmd>Git log<CR>', desc = 'Git Log'},
-        {'<leader>gs', '<cmd>horizontal Git status<CR>', desc = 'Git Status'},
-    }
+        { "<leader>gP", "<cmd>Git push<CR>",                              desc = "Git Push" },
+        { "<leader>ga", "<cmd>Git add *<CR>",                             desc = "Git Add" },
+        { "<leader>gc", "<cmd>horizontal Git commit<CR>",                 desc = "Git Commit" },
+        { "<leader>gh", "<cmd>vertical lua MiniGit.show_at_cursor()<CR>", desc = "Git Show" },
+        { "<leader>gl", "<cmd>Git log<CR>",                               desc = "Git Log" },
+        { "<leader>gs", "<cmd>horizontal Git status<CR>",                 desc = "Git Status" },
+    },
 }
