15791731fbef1b58a31c53ae0dc41b025a71f944
Eleazar Pequeno <edpu0033@gmail.com>
2025-11-20 23:57:19 -0500

    Refactor

--- neovim/.config/nvim/lua/config/settings.lua
+++ neovim/.config/nvim/lua/config/settings.lua
@@ -1,54 +1,54 @@
 local set = vim.opt
 
-set.backup = false                        -- Create a backup file
-set.breakindent = true                    -- Wrapped lines will continue indented
-set.clipboard = "unnamedplus"             -- Use the system clipboard
-set.colorcolumn = "80"                    -- Mark the 80th column
+set.backup = false -- Create a backup file
+set.breakindent = true -- Wrapped lines will continue indented
+set.clipboard = "unnamedplus" -- Use the system clipboard
+set.colorcolumn = "80" -- Mark the 80th column
 set.completeopt = "menu,menuone,noselect" -- Shows the pop up menu even there is only one option and selects nothing
-set.conceallevel = 0                      -- `` is visible in markdown files
-set.confirm = true                        -- Confirm to save change before exiting a modified buffer
-set.cursorline = true                     -- Highlight current line
-set.expandtab = true                      -- Convert tabs to spaces
-set.fileencoding = "utf-8"                -- The encoding written to a file
-set.foldenable = false                    -- All folds are open
-set.formatoptions = "jcroqlnt"            -- tcqj
-set.grepformat = "%f:%l:%c:%m"            --
-set.hidden = true                         -- Buffer becomes hidden when abandoned
-set.hlsearch = false                      -- Turn off search highlight
-set.ignorecase = true                     -- Ignore case in search patterns
-set.incsearch = true                      -- Highlight search pattern as it's type
-set.mouse = "a"                           -- Enable mouse support
-set.number = true                         -- Print a number in front of each line
-set.numberwidth = 2                       -- Set number column width to 2
-set.pumblend = 10                         -- Popup blend
-set.pumheight = 10                        -- Pop up menu height
-set.relativenumber = true                 -- Set relative numbered lines
-set.scrolloff = 9                         -- Minimal number of lines keept above or below the cursor
-set.shiftround = true                     -- Round indent.
-set.shiftwidth = 4                        -- The number of spaces inserted for each indentation
-set.showmode = true                       -- Show mode in cmd line
-set.sidescrolloff = 9                     -- Minimal number of columns keept right or left the cursor
-set.signcolumn = "yes"                    -- Always show the sign column
-set.smartcase = true                      -- Override ignorecase if the search pattern contains an uppercase
-set.smartindent = true                    -- Smart indent
-set.smarttab = true                       -- A <TAB> in front of a line inserts blanks acording to swiftwidth
-set.splitbelow = true                     -- Force horizontal splits to open below current buffer
-set.splitright = true                     -- Force vertical splits to open to the right of the current buffer
-set.swapfile = false                      -- Create swap file
-set.tabstop = 4                           -- Insert 4 spaces for tab
-set.termguicolors = true                  -- Enable 24-bits rgb in the TUI
-set.timeout = true                        -- Set on the wait between keymaps
-set.timeoutlen = 300                      -- Set on the time of the wait between keymaps
+set.conceallevel = 0 -- `` is visible in markdown files
+set.confirm = true -- Confirm to save change before exiting a modified buffer
+set.cursorline = true -- Highlight current line
+set.expandtab = true -- Convert tabs to spaces
+set.fileencoding = "utf-8" -- The encoding written to a file
+set.foldenable = false -- All folds are open
+set.formatoptions = "jcroqlnt" -- tcqj
+set.grepformat = "%f:%l:%c:%m" --
+set.hidden = true -- Buffer becomes hidden when abandoned
+set.hlsearch = false -- Turn off search highlight
+set.ignorecase = true -- Ignore case in search patterns
+set.incsearch = true -- Highlight search pattern as it's type
+set.mouse = "a" -- Enable mouse support
+set.number = true -- Print a number in front of each line
+set.numberwidth = 2 -- Set number column width to 2
+set.pumblend = 10 -- Popup blend
+set.pumheight = 10 -- Pop up menu height
+set.relativenumber = true -- Set relative numbered lines
+set.scrolloff = 9 -- Minimal number of lines keept above or below the cursor
+set.shiftround = true -- Round indent.
+set.shiftwidth = 4 -- The number of spaces inserted for each indentation
+set.showmode = true -- Show mode in cmd line
+set.sidescrolloff = 9 -- Minimal number of columns keept right or left the cursor
+set.signcolumn = "yes" -- Always show the sign column
+set.smartcase = true -- Override ignorecase if the search pattern contains an uppercase
+set.smartindent = true -- Smart indent
+set.smarttab = true -- A <TAB> in front of a line inserts blanks acording to swiftwidth
+set.splitbelow = true -- Force horizontal splits to open below current buffer
+set.splitright = true -- Force vertical splits to open to the right of the current buffer
+set.swapfile = false -- Create swap file
+set.tabstop = 4 -- Insert 4 spaces for tab
+set.termguicolors = true -- Enable 24-bits rgb in the TUI
+set.timeout = true -- Set on the wait between keymaps
+set.timeoutlen = 300 -- Set on the time of the wait between keymaps
 -- set.undodir = '$HOME/nvim/undodir'
-set.undofile = true                       -- Creates an undofile
-set.undolevels = 10000                    --
-set.updatetime = 250                      --
-set.wildmode = "longest:full,full"        -- Command-line completion mode
-set.wrap = true                           -- Display lines as one long line
-set.writebackup = false                   -- If a file is being edited by another program, it is not allowed to be edited
-set.list = true                           -- Sets how neovim will display certain whitespaces characters in the editor
+set.undofile = true -- Creates an undofile
+set.undolevels = 10000 --
+set.updatetime = 250 --
+set.wildmode = "longest:full,full" -- Command-line completion mode
+set.wrap = true -- Display lines as one long line
+set.writebackup = false -- If a file is being edited by another program, it is not allowed to be edited
+set.list = true -- Sets how neovim will display certain whitespaces characters in the editor
 set.listchars = { tab = ">> ", trail = ".", nbsp = "_" }
-set.inccommand = "split"                  -- Preview substitutions live, as you type!
+set.inccommand = "split" -- Preview substitutions live, as you type!
 set.autochdir = true
 
 vim.g.have_nerd_font = true -- Set to true if you have a Nerd Font installed and selected in the terminal
@@ -56,87 +56,87 @@ vim.g.have_nerd_font = true -- Set to true if you have a Nerd Font installed and
 vim.g.markdown_recommended_style = 0
 
 vim.api.nvim_create_autocmd("FileType", {
-    pattern = "markdown",
-    callback = function()
-        vim.treesitter.start()
-        vim.cmd("setlocal textwidth=80")
-        vim.cmd("setlocal wrap linebreak")
-        vim.cmd("setlocal spell spelllang=en_us")
-    end,
+	pattern = "markdown",
+	callback = function()
+		vim.treesitter.start()
+		vim.cmd("setlocal textwidth=80")
+		vim.cmd("setlocal wrap linebreak")
+		vim.cmd("setlocal spell spelllang=en_us")
+	end,
 })
 
 vim.api.nvim_create_autocmd("InsertEnter", {
-    pattern = "*",
-    callback = function()
-        vim.opt.relativenumber = false
-    end,
+	pattern = "*",
+	callback = function()
+		vim.opt.relativenumber = false
+	end,
 })
 
 vim.api.nvim_create_autocmd("InsertLeave", {
-    pattern = "*",
-    callback = function()
-        vim.opt.relativenumber = true
-    end,
+	pattern = "*",
+	callback = function()
+		vim.opt.relativenumber = true
+	end,
 })
 
 -- [[ Highlight on yank ]]
 -- See `:help vim.highlight.on_yank()`
 local highlight_group = vim.api.nvim_create_augroup("YankHighlight", { clear = true })
 vim.api.nvim_create_autocmd("TextYankPost", {
-    callback = function()
-        vim.highlight.on_yank()
-    end,
-    group = highlight_group,
-    pattern = "*",
+	callback = function()
+		vim.highlight.on_yank()
+	end,
+	group = highlight_group,
+	pattern = "*",
 })
 -- [[]]
 
 -- [[ Remove trailing whitespace on save ]]
 vim.api.nvim_create_autocmd("BufWritePre", {
-    pattern = "*",
-    command = "%s/\\s\\+$//e",
+	pattern = "*",
+	command = "%s/\\s\\+$//e",
 })
 -- [[]]
 
 -- [[ Smart auto-save with update command ]]
 vim.api.nvim_create_autocmd({ "BufLeave", "FocusLost" }, {
-    pattern = "*",
-    command = "silent! update",
-    desc = "Auto-save on leave/lost focus",
+	pattern = "*",
+	command = "silent! update",
+	desc = "Auto-save on leave/lost focus",
 })
 -- [[]]
 
 vim.g.markdown_fenced_languages = {
-    "html",
-    "javascript",
-    "typescript",
-    "css",
-    "scss",
-    "lua",
-    "vim",
-    "python",
-    "bash",
+	"html",
+	"javascript",
+	"typescript",
+	"css",
+	"scss",
+	"lua",
+	"vim",
+	"python",
+	"bash",
 }
 
 vim.diagnostic.config({
-    virtual_text = false,
-    virtual_lines = { only_current_line = true },
+	virtual_text = false,
+	virtual_lines = { only_current_line = true },
 })
 
 -- [[ No auto continue comments on new line ]]
 vim.api.nvim_create_autocmd("FileType", {
-    group = vim.api.nvim_create_augroup("no_auto_comment", {}),
-    callback = function()
-        vim.opt_local.formatoptions:remove({ "c", "r", "o" })
-    end,
+	group = vim.api.nvim_create_augroup("no_auto_comment", {}),
+	callback = function()
+		vim.opt_local.formatoptions:remove({ "c", "r", "o" })
+	end,
 })
 -- [[]]
 
 -- [[ Autoformat when saving ]]
 vim.api.nvim_create_autocmd("BufWritePre", {
-    pattern = "*",
-    callback = function()
-        vim.lsp.buf.format()
-    end,
+	pattern = "*",
+	callback = function()
+		vim.lsp.buf.format()
+	end,
 })
 -- [[]]
